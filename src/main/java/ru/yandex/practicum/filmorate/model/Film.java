8f046d78b8944c860d310311c94bb193cd6c1646
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-10-17 00:27:51 +0700

    check, differences in film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Data
 @Builder
+@RequiredArgsConstructor
 public class Film {
     private long id;
 
